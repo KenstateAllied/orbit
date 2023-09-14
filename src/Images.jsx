06bd9ce14d8eb34e11e4052f6ff2d6f1321f790b
Sam Muttai <42063584+muttaisammy@users.noreply.github.com>
2023-09-14 13:59:43 +0300

    b

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,35 +1,10 @@
-//import MobileApps from "./assets/MobileApps/samsung.jpg";
-//import planetMobileAppsInternal from "./assets/MobileApps/planet-MobileApps-internal.svg";
 import MobileApps from "./assets/MobileApps/mobiledevices.jpg";
-
-//import  WebApps from "./assets/WebApps/server.jpg";
-//import planetWebAppsInternal from "./assets/WebApps/planet-WebApps-internal.svg";
 import WebApps from "./assets/WebApps/WebDev.jpg";
-
-//import planetSystemsDev from "./assets/SystemsDev/planet-SystemsDev.svg";
-//import planetSystemsDevInternal from "./assets/SystemsDev/planet-SystemsDev-internal.svg";
 import SystemsDev from "./assets/SystemsDev/SystemsDev.jpg";
-
-//import planetBusinessIntel from "./assets/BusinessIntel/planet-BusinessIntel.svg";
-//import planetBusinessIntelInternal from "./assets/BusinessIntel/planet-BusinessIntel-internal.svg";
-
-import BusinessIntel from "./assets/BusinessIntel/BI_crucial.jpg";
-//import BusinessIntel from "./assets/BusinessIntel/BI_crucial.jpg";
-
-//import planetCloudComputing from "./assets/CloudComputing/planet-CloudComputing.svg";
-//import planetCloudComputingInternal from "./assets/CloudComputing/planet-CloudComputing-internal.svg";
+import BusinessIntel from "./assets/BusinessIntel/BI.jpg";
 import CloudComputing from "./assets/CloudComputing/Cloud-Computing.jpg";
-
-//import planetCyberSecurity from "./assets/CyberSecurity/planet-CyberSecurity.svg";
-//import planetCyberSecurityInternal from "./assets/CyberSecurity/planet-CyberSecurity-internal.svg";
 import CyberSecurity from "./assets/CyberSecurity/sec.jpg";
-
-//import planetTechSales from "./assets/TechSales/planet-TechSales.svg";
-//import planetTechSalesInternal from "./assets/TechSales/planet-TechSales-internal.svg";
 import TechSales from "./assets/TechSales/TechSales.jpg";
-
-//import planetTraining from "./assets/Training/planet-Training.svg";
-//import planetTrainingInternal from "./assets/Training/planet-Training-internal.svg";
 import Training from "./assets/Training/laptop.jpg";
 
 export const images = [
